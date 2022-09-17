c389f8b0a79750b30268032c42230db080de0292
pv18 <pv9998286620@yandex.ru>
2022-09-17 15:08:29 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from 'antd';
+
+const App = () => {
+    return (
+        <div>
+            <Button>add</Button>
+        </div>
+    );
+};
+
+export default App;
